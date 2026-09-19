1749aaeb7f2e8755161a40d080fc75b08d04484e
rikomuze <logical.bear.ishi@gmail.com>
2026-09-19 13:58:17 +0900

    Add MUZE PROFILE song and live data

---
+++ muze-profile/data.js
@@ -0,0 +1,19 @@
+window.MUZE_PROFILE_DATA={
+members:["KAIRYU","NAOYA","RAN","SEITO","RYUKI","TAKUTO","HAYATO","EIKI"],
+songs:[
+"MISSION","Vivid","CAME TO DANCE","Fantasy","Carnival","LIGHTNING","Fire","Holiday","Get Down","Parade","K&K","Waterfall","Ain’t no fun","1st Date","ICE feat. REIKO","Love Letter","Counterattack","Seaside Story","MAZQUERADE","Our Life Is Always Right","King Kila Game","J.O.K.E.R.","I’m yours, You’re mine","to me","HERO SUIT","DANGER","Only You","Trap","BANQUET BANG","Get Up And Dance","T.O.P","MAKERZ","Interlude ~Back On The Summer Day~","Clover","The Voice","So Strawberry","Touch","Round 12"
+],
+lives:[
+"MAZZEL DEBUT SHOWCASE “Vivid”",
+"MAZZEL 1st Fan Meeting -Play at the MUZEUM-",
+"MAZZEL SHOWCASE “Carnival”",
+"MAZZEL SHOWCASE “Parade”",
+"MAZZEL 1st One Man Tour 2024 “Join us in the PARADE”",
+"MAZZEL 1st One Man Tour 2024 “Join us in the PARADE” Arena",
+"MAZZEL 2nd One Man Tour 2025 “Royal Straight Flush”",
+"MAZZEL 1st Arena Tour 2026 “Shall we hit the Banquet?”",
+"MAZZEL 2nd Fan Meeting -Play at the MUZEUM Vol.2-",
+"その他"
+],
+areas:["北海道","東北","関東","中部","関西","中国","四国","九州・沖縄"]
+};
